ce5cbe79ebb4dd7335fe5514d2fd4ed6c62b2b68
MasqueradeOfSilence <alexcn711@gmail.com>
2025-10-24 10:30:50 -0600

    XCode begins

---
+++ TicTacToe/TicTacToe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TicTacToe
+//
+//  Created by Alex Neville on 10/24/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    std::cout << "Hello, Tic Tac Toe!\n";
+    return EXIT_SUCCESS;
+}
