0e22f1195e3197f9e2ba117bfbb39d4ba7df76e2
MasqueradeOfSilence <alexcn711@gmail.com>
2025-12-06 21:55:08 -0700

    WIP player superclass

---
+++ TicTacToe/TicTacToe/Player.cpp
@@ -0,0 +1,9 @@
+//
+//  Player.cpp
+//  TicTacToe
+//
+//  Created by Alex Neville on 12/6/25.
+//
+
+#include "Player.hpp"
+#include <iostream>
---
+++ TicTacToe/TicTacToe/Player.hpp
@@ -0,0 +1,20 @@
+//
+//  Player.hpp
+//  TicTacToe
+//
+//  Created by Alex Neville on 12/6/25.
+//
+
+#ifndef Player_hpp
+#define Player_hpp
+
+#include <stdio.h>
+
+class Player
+{
+public:
+    
+private:
+};
+
+#endif /* Player_hpp */
